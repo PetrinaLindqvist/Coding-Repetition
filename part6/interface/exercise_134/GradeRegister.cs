b80218e00bf04664763b4783169c1514b0b3fc85
Petrina Lindqvist <petrina.lindqvist@centria.fi>
2020-08-19 10:41:30 +0300

    Part 6 done

--- part6/interface/exercise_134/GradeRegister.cs
+++ part6/interface/exercise_134/GradeRegister.cs
@@ -2,70 +2,99 @@ using System.Collections.Generic;
 using System;
 public class GradeRegister
 {
-  private List<int> grades;
+    private List<int> grades;
+    private List<int> points;
 
-  public GradeRegister()
-  {
-    this.grades = new List<int>();
-  }
+    public GradeRegister()
+    {
+        this.grades = new List<int>();
+        this.points = new List<int>();
+        
+    }
+    public void AddGradeBasedOnPoints(int points)
+    {
+        this.grades.Add(PointsToGrades(points));
+        this.points.Add(points);
+    }
 
-  public void AddGradeBasedOnPoints(int points)
-  {
-    this.grades.Add(PointsToGrades(points));
-  }
+    public int NumberOfGrades(int grade)
+    {
+        int count = 0;
+        foreach (int received in this.grades)
+        {
+            if (received == grade)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 
-  public int NumberOfGrades(int grade)
-  {
-    int count = 0;
-    foreach (int received in this.grades)
+    public static int PointsToGrades(int points)
     {
-      if (received == grade)
-      {
-        count++;
-      }
+        int grade = 0;
+        if (points < 50)
+        {
+            grade = 0;
+        }
+        else if (points < 60)
+        {
+            grade = 1;
+        }
+        else if (points < 70)
+        {
+            grade = 2;
+        }
+        else if (points < 80)
+        {
+            grade = 3;
+        }
+        else if (points < 90)
+        {
+            grade = 4;
+        }
+        else
+        {
+            grade = 5;
+        }
+        return grade;
     }
-    return count;
-  }
 
-  public static int PointsToGrades(int points)
-  {
-    int grade = 0;
-    if (points < 50)
+    public double AverageOfGrades()
     {
-      grade = 0;
-    }
-    else if (points < 60)
-    {
-      grade = 1;
-    }
-    else if (points < 70)
-    {
-      grade = 2;
-    }
-    else if (points < 80)
-    {
-      grade = 3;
-    }
-    else if (points < 90)
-    {
-      grade = 4;
-    }
-    else
-    {
-      grade = 5;
-    }
-    return grade;
-  }
+        // Hint! You don't need to round the -1, but you do need it for all the other results...
+        
+        if (this.grades.Count == 0)
+        {
+          return -1;
+        }
+        int sum = 0;
+        foreach(int givenGrade in grades)
+        {
+          sum += givenGrade;
+        }
+        double average = (double)sum / this.grades.Count;
+        return Math.Round(average, 2);
+        
 
-  public double AverageOfGrades()
-  {
-    // Hint! You don't need to round the -1, but you do need it for all the other results...
-    return Math.Round(-1.0, 2);
-  }
+    }
 
-  public double AverageOfPoints()
-  {
-
-    return Math.Round(-1.0, 2);
-  }
+    public double AverageOfPoints()
+    {
+      if (this.points.Count == 0)
+        {
+          return -1;
+        }
+        int sum = 0;
+        foreach(int givenPoints in points)
+        {
+          sum += givenPoints;
+        }
+        double average = (double)sum / this.points.Count;
+        {
+          return Math.Round(average, 2);
+        }
+        
+        
+    }
 }
--- part6/interface/exercise_134/Program.cs
+++ part6/interface/exercise_134/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 
 namespace exercise_134
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      GradeRegister register = new GradeRegister();
+        public static void Main(string[] args)
+        {
+            GradeRegister register = new GradeRegister();
+           
+            UserInterface userinterface = new UserInterface(register);
+            userinterface.Start();
 
-      UserInterface ui = new UserInterface(register);
-      ui.Start();
+            Console.WriteLine("The average of points: " + register.AverageOfPoints());
+            Console.WriteLine("The average of grades: " + register.AverageOfGrades());
+        }
     }
-  }
 }
